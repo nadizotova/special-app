b05e08a6000e21b295857687857c5088247f7d60
nadezhda_zotova <zotowwwwa@gmail.com>
2022-11-19 14:00:43 +0500

    adedd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Дата и время: ");
+        System.out.println(LocalDateTime.now());
     }
 }
